4d9b5e6ceda5de6809b31c2c947d2c35e333f2ee
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 16:48:59 +0300

    updated models.py == created class Profile

--- instagram/models.py
+++ instagram/models.py
@@ -3,3 +3,15 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = CloudinaryField('images/')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
