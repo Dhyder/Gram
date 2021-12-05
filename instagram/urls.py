31599a39fd4875f79e6846a8da8829acf395cd22
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 17:13:55 +0300

    updated urls.py == created user_profile path/ post/ path post<id>/like path

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('account/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
     path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('post/<id>', views.post_comment, name='comment'),
+    path('post/<id>/like', PostLikeToggle.as_view(), name='liked'),
+
 ]
