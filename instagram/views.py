91ae9e8134ce42713f9b1ca6c73cfbe185bdc10e
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 17:03:43 +0300

    updated views.py == def signup

--- instagram/views.py
+++ instagram/views.py
@@ -10,3 +10,18 @@ from rest_framework.response import Response
 from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
 from rest_framework import authentication, permissions
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
