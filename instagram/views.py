c78082d41bd57215cb90e93e602ec3505bad1957
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 16:59:46 +0300

    updated views.py == import from forms & django.contrib.auth

--- instagram/views.py
+++ instagram/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, JsonResponse
 from rest_framework.views import APIView
 from django.views.generic import RedirectView
+from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
+from django.contrib.auth import login, authenticate
