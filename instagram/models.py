9779df00a1f912b47140c696b0544fe1848add05
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 16:50:28 +0300

    updated models.py == Created classmethods receiver && def save profile && delete profile

--- instagram/models.py
+++ instagram/models.py
@@ -15,3 +15,22 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
