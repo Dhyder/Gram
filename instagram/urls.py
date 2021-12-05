781d879b3d95a38061c81426c59a30a03f46cfba
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 17:14:37 +0300

    updated urls.py == created like path/ and search path

--- instagram/urls.py
+++ instagram/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('user_profile/<username>/', views.user_profile, name='user_profile'),
     path('post/<id>', views.post_comment, name='comment'),
     path('post/<id>/like', PostLikeToggle.as_view(), name='liked'),
+    path('like', views.like_post, name='like_post'),
+    path('search/', views.search_profile, name='search'),
 
 ]
