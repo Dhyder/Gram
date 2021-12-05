f59cd6bca97edaa7af5946cd5de529f85d39c2ba
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 16:48:19 +0300

    updated models.py == import CloudinaryField

--- instagram/models.py
+++ instagram/models.py
@@ -2,3 +2,4 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
