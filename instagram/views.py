525fbeb2b6b9a98ed8f0fe0948829136f030d271
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 16:58:49 +0300

    updated views.py == import Api view & Redirect view

--- instagram/views.py
+++ instagram/views.py
@@ -2,3 +2,5 @@ from django.contrib.auth.models import User
 from .models import Post, Comment, Profile, Follow
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, JsonResponse
+from rest_framework.views import APIView
+from django.views.generic import RedirectView
