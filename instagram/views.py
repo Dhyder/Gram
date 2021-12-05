4e9409875510dd6e671a5b72dcc8258a0a9c1edc
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-05 16:58:12 +0300

    updated views.py == import from django.http & django.shortcuts

--- instagram/views.py
+++ instagram/views.py
@@ -1,2 +1,4 @@
 from django.contrib.auth.models import User
 from .models import Post, Comment, Profile, Follow
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect, JsonResponse
